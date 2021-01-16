13879d2b753ebe8bfdcf4717fa727b730aaa9dce
LƯU NGUYỄN HOÀNG <luuhoang231@gmail.com>
2021-01-16 14:42:57 +0700

    login with google and facebook

--- customers/src/views/Login.js
+++ customers/src/views/Login.js
@@ -1,28 +1,36 @@
 import React, { useEffect } from "react";
 import "../styles/login/login.scss";
 import logo from "../assets/images-2.png";
-import { useHistory } from 'react-router-dom';
+import { useHistory } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { signTest, updateHeaderFooter } from "../redux/users/actions";
-
+import FacebookLogin from "react-facebook-login/dist/facebook-login-render-props";
+import GoogleLogin from "react-google-login";
 export default function Login(props) {
   const history = useHistory();
   const dispatch = useDispatch();
-  const user = {
-    name: 'Phạm Quang Duy',
-    email: 'example@example.com'
-  }
-  const login = () => {
-    dispatch(signTest(user, history))
-  }
 
+  const user = {
+    name: "Phạm Quang Duy",
+    email: "example@example.com",
+  };
+  const login = () => {};
+  const responseFacebook = (response) => {
+    console.log("response", response);
+  };
+
+  const responseGoogle = (response) => {
+    console.log(response);
+  };
   useEffect(() => {
-    dispatch(updateHeaderFooter({
-      header: false,
-      footer: false
-    }))
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [])
+    dispatch(
+      updateHeaderFooter({
+        header: false,
+        footer: false,
+      })
+    );
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
   return (
     <div className="login">
       <div className="login-background">
@@ -33,15 +41,37 @@ export default function Login(props) {
           </div>
           <div className="login-form__groups">
             <div className="groups-btn">
-              <button onClick={login} className="btn btn-facebook">
-                Login with Facebook
-              </button>
+              <FacebookLogin
+                appId="409505490301267"
+                callback={responseFacebook}
+                render={(renderProps) => (
+                  <button
+                    onClick={renderProps.onClick}
+                    className="btn btn-facebook"
+                  >
+                    Login with Facebook
+                  </button>
+                )}
+              />
+
               <button onClick={login} className="btn btn-zalo">
                 Login with Zalo
               </button>
-              <button onClick={login} className="btn btn-google">
-                Login with Google
-              </button>
+              <GoogleLogin
+                clientId="827380774255-d0ksdk40jlq25n7g7okdgdl7hm073iip.apps.googleusercontent.com"
+                onSuccess={responseGoogle}
+                onFailure={responseGoogle}
+                cookiePolicy={"single_host_origin"}
+                render={(renderProps) => (
+                  <button
+                    onClick={renderProps.onClick}
+                    disabled={renderProps.disabled}
+                    className="btn btn-google"
+                  >
+                    Login with Google
+                  </button>
+                )}
+              />
             </div>
           </div>
         </div>
